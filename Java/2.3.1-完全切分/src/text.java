6285cbb454e70eb9b03365efc15ef3e28861ff0e
C-Wwb <1729220020@qq.com>
2020-03-28 23:02:31 +0800

    20.03.28

--- Java/2.3.1-完全切分/src/text.java
+++ Java/2.3.1-完全切分/src/text.java
@@ -9,7 +9,10 @@ public class text
     public static void main(String[] args) throws IOException {//读取词典
         TreeMap<String, CoreDictionary.Attribute> dictionary
                 = IOUtil.loadDictionary("Y:/NLP/Anaconda/Install/Lib/site-packages/pyhanlp/static/data/dictionary/CoreNatureDictionary.mini.txt");
-        System.out.println(Main.segmentFully("就读于北京信息科技大学", dictionary));
+        System.out.println(Main.segmentFully("我和杜玉洁就读于北京信息科技大学", dictionary));
 
     }
 }
+//text 待分词的文本
+//dictionary 词典，一定要先加载词典
+//return 单词列表
