bcb0b37e673a1a28ce49a9da9ee3075524580737
C-Wwb <1729220020@qq.com>
2020-04-18 22:36:41 +0800

    20.04.18

--- Java/2.2.2-加载词典/src/Mian.java
+++ Java/2.2.2-加载词典/src/Mian.java
@@ -12,4 +12,12 @@ public class Mian
         System.out.printf("词典大小: %d个词条\n", dictionary.size());
         System.out.println(dictionary.keySet().iterator().next());
     }
+    /*public static void main(String[] args) throws IOException
+    {
+        String trainWords = MSR.TRAIN_WORDS;
+        Segment segment = new DoubleArrayTrieSegment(trainWords)
+                .enablePartOfSpeechTagging(true);
+        CWSEvaluator.Result result = CWSEvaluator.evaluate(segment, MSR.TEST_PATH, MSR.OUTPUT_PATH, MSR.GOLD_PATH, trainWords);
+        System.out.println(result);
+    }*/
 }
--- Python/2.9-准确率测评/2.9.7.ConfusionMatrix.py
+++ Python/2.9-准确率测评/2.9.7.ConfusionMatrix.py
@@ -13,19 +13,35 @@ def to_region(segmentation: str) -> list:
         start = end
     return region
 
-def prf(gold: str, pred: str) -> tuple:
+def prf(gold: str, pred: str, dic) -> tuple:
     """
     计算P\R\F1
     :param gold: 标准答案文件，比如“商品 和 服务”
     :param pred: 分词结束文件，比如“商品 和服 务”
-    :return: （P,R,F1）
+    :param dic: 词典
+    :return: （P, R, F1, OOV_R, IV_R）
     """
-    A_size, B_size, A_cap_B_size = 0, 0, 0
+    A_size, B_size, A_cap_B_size, OOV, IV, OOV_R, IV_R = 0, 0, 0, 0, 0, 0, 0
     with open(gold) as gd, open(pred) as pd:
         for g, p in zip(gd, pd):
             A, B = set(to_region(g), set(to_region(p)))
             A_size += len(A)
             B_size += len(B)
             A_cap_B_size += len(A & B)
-    p, r = A_cap_B_size / B_size, A_cap_B_size / A_size
-    return p, r, 2 * p * r / (p + r)
+            import re
+            text = re.sub("\\s+", "", g)
+            for(start, end) in A:
+                word = text[start: end]
+                if dic.containsKey(word):
+                    IV += 1
+                else:
+                    OOV += 1
+
+            for (start, end) in A & B:
+                word = text[start: end]
+                if dic.containsKey(word):
+                    IV_R += 1
+                else:
+                    OOV_R += 1
+    p, r = A_cap_B_size / B_size * 100, A_cap_B_size / A_size * 100
+    return p, r, 2 * p * r / (p + r), OOV_R / OOV * 100, IV_R / IV * 100
