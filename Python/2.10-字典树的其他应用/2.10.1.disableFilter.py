f0cc23ed82fa19b5a8dbe36fe1db1081a80af444
C-Wwb <1729220020@qq.com>
2020-04-20 23:38:21 +0800

    20.04.20

---
+++ Python/2.10-字典树的其他应用/2.10.1.disableFilter.py
@@ -0,0 +1,27 @@
+from jpype import JClass
+
+
+def load_from_file(path):
+    """
+    从词典文件中加载DoubleArrayTrie
+    :param path: 词典路径
+    :return: 双数组字典树
+    """
+
+    map = JClass('java.util.TreeMap')() #创建treemap实例
+    with open(path) as src:
+        for word in src:
+            word = word.strip() #去掉python读入的\n
+            map[word] = word
+    return JClass('com.hancks.hanlp.collection.trie.DoubleArrayTrie')(map)
+
+def load_from_words(*words):
+    """
+    从词汇构造双数组字典树
+    :param words: 一系列词语
+    :return:
+    """
+    map = JClass('java.util.TreeMap')() #创建treemap实例
+    for word in words:
+        map[word] = word
+    return JClass('com.hancks.hanlp.collection.trie.DoubleArrayTrie')(map)
