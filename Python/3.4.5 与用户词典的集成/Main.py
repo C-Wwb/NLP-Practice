751a81da33adb085b677c756e58e78cf92bb3e0b
C-Wwb <1729220020@qq.com>
2020-06-02 21:03:49 +0800

    20.06.02

---
+++ Python/3.4.5 与用户词典的集成/Main.py
@@ -0,0 +1,10 @@
+from pyhanlp import *
+segment = ViterbiSegment()
+sentence = "社会摇摆简称社会摇"
+segment.enableCustomDictionary(False)
+print("不挂载词典：", segment.seg(sentence))
+CustomDictionary.insert("社会摇", "nz 100")
+segment.enableCustomDictionary(True)
+print("低优先级词典：", segment.seg(sentence))
+segment.enableCustomDictionaryForcing(True)
+print("高优先级词典：", segment.seg(sentence))
